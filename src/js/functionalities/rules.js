388b18005518f550bec0e0f6838d234a6281a90f
Daniel Santos <dsantos0495@gmail.com>
2020-06-29 18:46:38 -0500

    Se termina animaciones de modal rules

--- src/js/functionalities/rules.js
+++ src/js/functionalities/rules.js
@@ -3,15 +3,27 @@ const rules = () => {
   const rulesClose = document.getElementById('rules__close');
   const rulesOpen = document.getElementById('rules__button');
   const rulesOverlay = document.getElementById('rules__overlay');
+  const rulesModal = document.getElementById('rules__modal');
 
   function rulesTogle() {
 
     if (rulesOverlay.classList.contains('is-active-f')) {
 
-      rulesOverlay.classList.remove('is-active-f');
+      function removeModal(e) {
+
+          rulesOverlay.classList.remove('is-active-f');
+          rulesModal.removeEventListener('animationend', removeModal);
+
+      }
+
+      rulesOverlay.classList.add('out');
+      rulesModal.classList.add('out');
+      rulesModal.addEventListener('animationend', removeModal);
 
     } else {
 
+      rulesModal.classList.remove('out');
+      rulesOverlay.classList.remove('out');
       rulesOverlay.classList.add('is-active-f');
 
     }
